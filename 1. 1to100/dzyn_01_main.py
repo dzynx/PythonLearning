9330d4616a721f81959d18556b60a8be0063e3ef
dzynx <45103983+dzynx@users.noreply.github.com>
2024-08-28 17:48:31 +0100

    Update Uprzątanie dzyn_01_main.py

--- 1. 1to100/dzyn_01_main.py
+++ 1. 1to100/dzyn_01_main.py
@@ -8,21 +8,21 @@
 # (gdy trafiona to info ile było prób)
 
 # robienie liczby losowej
-import random
-liczba_wylosowana_przez_komp = random.randint(1, 101)
-print(liczba_wylosowana_przez_komp)
+from random import randint
+liczba_wylosowana_przez_komp = randint(1, 101)
 
 # licznik ile razy użytkownik próbował zgadnąć liczbę losową
 ile_razy_zgadywal = 0
 
-#inicjalizaja zmiennej - liczby podawanej przez ludzia
+# inicjalizaja zmiennej - liczby podawanej przez ludzia
 liczba_zgadywana_przez_ludzia = 102
-#robienie w pętli, żeby user zgadywał i zgadywał aż zgadnie
+# robienie w pętli, żeby user zgadywał i zgadywał aż zgadnie
+# ma być robione, dopóki użytkownik nie zgadnie liczby losowej
 while liczba_wylosowana_przez_komp != liczba_zgadywana_przez_ludzia:
     # branie liczby od ludzia
     # dane pobrane inputem trzeeba z-int()ować, bo dane z klawwiatury to chyba string domyślnie
     liczba_zgadywana_przez_ludzia = int(input('zgadnij jaką liczbę wylosował komputer: '))
-    print(liczba_zgadywana_przez_ludzia)
+    print(f'Tym razem podano lczbę: {liczba_zgadywana_przez_ludzia}')
 
     # porównywanie liczby z kompa z liczbą od ludzia
 
@@ -31,9 +31,6 @@ while liczba_wylosowana_przez_komp != liczba_zgadywana_przez_ludzia:
         print('lecisz za nisko ze zgadywaniem')
     elif liczba_wylosowana_przez_komp < liczba_zgadywana_przez_ludzia:
         print('lecisz za wysoko ze zgadywaniem')
-    # ma być robione, dopóki użytkownik nie zgadnie liczby losowej
-    print(liczba_wylosowana_przez_komp == liczba_zgadywana_przez_ludzia)
     ile_razy_zgadywal += 1
 
-print(ile_razy_zgadywal)
-print('coś w ogóle zostało wykonane')
+print(f'Udało Ci się zgadnąć za {ile_razy_zgadywal} razem')
